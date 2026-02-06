005850dc271821e58b3e6db6bae669c665cce182
Sheryn Anand <sheryn606@gmail.com>
2026-02-06 19:18:10 +0530

    Added navbar and routing with preliminary styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',component: HomeComponent
+    },
+    {path:'products',component: ProductsComponent
+    },
+    {path:'cart',component: CartComponent
+    },
+    {path:'checkout',component: CheckoutComponent
+    },
+    {path:'about',component: AboutComponent
+    },
+    {path:'**',redirectTo:'home'}
+
+];
