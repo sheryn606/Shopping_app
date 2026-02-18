684677862001bc84c78a251622c755170ea83359
Sheryn Anand <sheryn606@gmail.com>
2026-02-18 11:48:34 +0530

    Conflicts resolved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { ProductsComponent } from './components/products/products.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { AboutComponent } from './components/about/about.component';
-import { LoginComponent } from './login/login.component';
+
 
 export const routes: Routes = [
     {path:'',redirectTo:'home',pathMatch:'full'},
@@ -18,7 +18,6 @@ export const routes: Routes = [
     },
     {path:'about',component: AboutComponent
     },
-    {path:'login',component:LoginComponent},
     {path:'**',redirectTo:'home'}
     
 
